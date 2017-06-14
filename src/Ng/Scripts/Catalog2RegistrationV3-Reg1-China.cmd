59cf586db2c35f6a6dc082535f01f509c1747cd5
Cristina Manu <cmanu@microsoft.com>
2017-06-13 22:34:21 -0700

    V3China

--- src/Ng/Scripts/Catalog2RegistrationV3-Reg1-China.cmd
+++ src/Ng/Scripts/Catalog2RegistrationV3-Reg1-China.cmd
@@ -3,12 +3,12 @@
 cd Ng
 
 :Top
-	echo "Starting job - #{Jobs.catalog2registrationv3reg1.China.Title}"
+	echo "Starting job - #{Jobs.catalog2registrationv3reg1China.Title}"
 
-	title #{Jobs.catalog2registrationv3reg1.China.Title}
+	title #{Jobs.catalog2registrationv3reg1China.Title}
 
-    start /w Ng.exe catalog2registration -source #{Jobs.common.v3.Source} -contentBaseAddress #{Jobs.catalog2registrationv3reg1.China.ContentBaseAddress} -storageBaseAddress #{Jobs.catalog2registrationv3reg1.China.StorageBaseAddress} -storageType azure -storageAccountName #{Jobs.common.v3.China.StorageAccountName} -storageKeyValue #{Jobs.common.v3.ChinaStorage.Key} -storageContainer #{Jobs.catalog2registrationv3reg1.StorageContainer} -useCompressedStorage #{Jobs.catalog2registrationv3reg1.UseCompressedStorage} -compressedStorageBaseAddress #{Jobs.catalog2registrationv3reg1.China.StorageBaseAddressCompressed} -compressedStorageAccountName #{Jobs.common.v3.China.StorageAccountName} -compressedStorageKeyValue #{Jobs.common.v3.ChinaStorage.Key} -compressedStorageContainer #{Jobs.catalog2registrationv3reg1.StorageContainerCompressed} -instrumentationkey #{Jobs.common.v3.Logging.InstrumentationKey} -vaultName #{Deployment.Azure.KeyVault.VaultName} -clientId #{Deployment.Azure.KeyVault.ClientId} -certificateThumbprint #{Deployment.Azure.KeyVault.CertificateThumbprint} -verbose true -interval #{Jobs.common.v3.Interval}
+    start /w Ng.exe catalog2registration -source #{Jobs.common.v3.Source} -contentBaseAddress #{Jobs.catalog2registrationv3reg1China.ContentBaseAddress} -storageBaseAddress #{Jobs.catalog2registrationv3reg1China.StorageBaseAddress} -storageType azure -storageAccountName #{Jobs.common.v3China.StorageAccountName} -storageKeyValue #{Jobs.common.v3ChinaStorage.Key} -storageContainer #{Jobs.catalog2registrationv3reg1.StorageContainer} -useCompressedStorage #{Jobs.catalog2registrationv3reg1.UseCompressedStorage} -compressedStorageBaseAddress #{Jobs.catalog2registrationv3reg1China.StorageBaseAddressCompressed} -compressedStorageAccountName #{Jobs.common.v3China.StorageAccountName} -compressedStorageKeyValue #{Jobs.common.v3ChinaStorage.Key} -compressedStorageContainer #{Jobs.catalog2registrationv3reg1.StorageContainerCompressed} -instrumentationkey #{Jobs.common.v3.Logging.InstrumentationKey} -vaultName #{Deployment.Azure.KeyVault.VaultName} -clientId #{Deployment.Azure.KeyVault.ClientId} -certificateThumbprint #{Deployment.Azure.KeyVault.CertificateThumbprint} -verbose true -interval #{Jobs.common.v3.Interval}
 
-	echo "Finished #{Jobs.catalog2registrationv3reg1.China.Title}"
+	echo "Finished #{Jobs.catalog2registrationv3reg1China.Title}"
 
 	goto Top
--- src/Ng/Scripts/Catalog2RegistrationV3-Reg2-China.cmd
+++ src/Ng/Scripts/Catalog2RegistrationV3-Reg2-China.cmd
@@ -3,13 +3,13 @@
 cd Ng
 
 :Top
-echo "Starting job - #{Jobs.catalog2registrationv3reg2.China.Title}"
+echo "Starting job - #{Jobs.catalog2registrationv3reg2China.Title}"
 
-title #{Jobs.catalog2registrationv3reg2.China.Title}
+title #{Jobs.catalog2registrationv3reg2China.Title}
 
 start /w ng.exe catalog2registration ^
     -source #{Jobs.common.v3.Source} ^
-    -contentBaseAddress #{Jobs.catalog2registrationv3reg2.China.ContentBaseAddress} ^
+    -contentBaseAddress #{Jobs.catalog2registrationv3reg2China.ContentBaseAddress} ^
     -storageType azure ^
     -instrumentationkey #{Jobs.common.v3.Logging.InstrumentationKey} ^
     -vaultName #{Deployment.Azure.KeyVault.VaultName} ^
@@ -17,21 +17,21 @@ start /w ng.exe catalog2registration ^
     -certificateThumbprint #{Deployment.Azure.KeyVault.CertificateThumbprint} ^
     -verbose true ^
     -interval #{Jobs.common.v3.Interval} ^
-    -storageBaseAddress #{Jobs.catalog2registrationv3reg2.China.StorageBaseAddress} ^
-    -storageAccountName #{Jobs.common.v3.China.StorageAccountName} ^
-    -storageKeyValue #{Jobs.common.v3.ChinaStorage.Key} ^
+    -storageBaseAddress #{Jobs.catalog2registrationv3reg2China.StorageBaseAddress} ^
+    -storageAccountName #{Jobs.common.v3China.StorageAccountName} ^
+    -storageKeyValue #{Jobs.common.v3ChinaStorage.Key} ^
     -storageContainer #{Jobs.catalog2registrationv3reg2.StorageContainer} ^
     -useCompressedStorage #{Jobs.catalog2registrationv3reg2.UseCompressedStorage} ^
-    -compressedStorageBaseAddress #{Jobs.catalog2registrationv3reg2.China.StorageBaseAddressCompressed} ^
-    -compressedStorageAccountName #{Jobs.common.v3.China.StorageAccountName} ^
-    -compressedStorageKeyValue #{Jobs.common.v3.ChinaStorage.Key} ^
+    -compressedStorageBaseAddress #{Jobs.catalog2registrationv3reg2China.StorageBaseAddressCompressed} ^
+    -compressedStorageAccountName #{Jobs.common.v3China.StorageAccountName} ^
+    -compressedStorageKeyValue #{Jobs.common.v3ChinaStorage.Key} ^
     -compressedStorageContainer #{Jobs.catalog2registrationv3reg2.StorageContainerCompressed} ^
     -useSemVer2Storage #{Jobs.catalog2registrationv3reg2.UseSemVer2Storage} ^
-    -semVer2StorageBaseAddress #{Jobs.catalog2registrationv3reg2.China.StorageBaseAddressSemVer2} ^
-    -semVer2StorageAccountName #{Jobs.common.v3.China.StorageAccountName} ^
-    -semVer2StorageKeyValue #{Jobs.common.v3.ChinaStorage.Key} ^
+    -semVer2StorageBaseAddress #{Jobs.catalog2registrationv3reg2China.StorageBaseAddressSemVer2} ^
+    -semVer2StorageAccountName #{Jobs.common.v3China.StorageAccountName} ^
+    -semVer2StorageKeyValue #{Jobs.common.v3ChinaStorage.Key} ^
     -semVer2StorageContainer #{Jobs.catalog2registrationv3reg2.StorageContainerSemVer2}
 
-echo "Finished #{Jobs.catalog2registrationv3reg2.China.Title}"
+echo "Finished #{Jobs.catalog2registrationv3reg2China.Title}"
 
 goto Top
