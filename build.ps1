e0dae5a333962ee5eb7dd4aaa50309102d00f84e
Scott Bommarito <scottbom@microsoft.com>
2018-10-18 15:40:40 -0700

    update buildbranch to specific commit

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ param (
     [string]$SemanticVersion = '1.0.0-zlocal',
     [string]$Branch,
     [string]$CommitSHA,
-    [string]$BuildBranch = 'sb-micro'
+    [string]$BuildBranch = 'a0a53ed81cd714fb8de708fd54ab0d09f6fc9671'
 )
 
 # For TeamCity - If any issue occurs, this script fails the build. - By default, TeamCity returns an exit code of 0 for all powershell scripts, even if they fail
